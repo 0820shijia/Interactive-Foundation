a56333ec84e622ef88d1396ddf073b02adbae3b7
0820shijia <Sluo@cca.edu>
2021-04-06 01:10:53 +0800

    graphic collection
    new update

---
+++ P2 Paratext Collection /Captcha Typefaces - Jiwon Park—work_files/template.js
@@ -0,0 +1,6 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function() {
+	$(document).trigger("pageReady");
+	changeHorizNav(1);
+	$(document).trigger("initNextProject");
+});
